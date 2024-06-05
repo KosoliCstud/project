2fbe83519bad25e2b6f0cf3700e85108d7a25045
Adam <adampekala48@gmail.com>
2024-06-05 22:49:50 +0200

    Project: service code added with dockerfile to provide real-time data scraping

---
+++ service_code.py
@@ -0,0 +1,75 @@
+import psycopg2
+import requests
+import pandas as pd
+import matplotlib.pyplot as plt
+import schedule
+import time
+
+db_name = 'gios_data'
+db_user = 'postgres'
+db_password = 'kochamadb'
+db_host = '172.31.48.1'
+db_port = '5000'
+
+conn = psycopg2.connect(
+    dbname=db_name,
+    user=db_user,
+    password=db_password,
+    host=db_host,
+    port=db_port
+)
+
+cur = conn.cursor()
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS air_quality(
+    station_id INTEGER,
+    measurement_date TIMESTAMP,
+    index_value DECIMAL,
+    index_name VARCHAR(255)
+);
+''')
+conn.commit()
+
+def get_all_ids():
+    response = requests.get("https://api.gios.gov.pl/pjp-api/rest/station/findAll")
+    data = response.json()
+
+    ids = [station['id'] for station in data]
+    return ids
+
+all_ids = get_all_ids()
+
+def fetch_data_from_api(api_url):
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+def insert_data_to_db(conn, data):
+    cur = conn.cursor()
+    if data['AqIndex']:
+        cur.execute('''
+            INSERT INTO air_quality (station_id, measurement_date, index_value, index_name)
+            VALUES (%s, %s, %s, %s);
+        ''', (
+            data['AqIndex']['Identyfikator stacji pomiarowej'],
+            data['AqIndex']['Data wykonania obliczeń indeksu'],
+            data['AqIndex']['Wartość indeksu'],
+            data['AqIndex']['Nazwa kategorii indeksu'],
+        ))
+        conn.commit()
+
+def job():
+    for station_id in all_ids:
+        api_url = f'https://api.gios.gov.pl/pjp-api/v1/rest/aqindex/getIndex/{station_id}'
+        data = fetch_data_from_api(api_url)
+        if data:
+            insert_data_to_db(conn, data)
+
+schedule.every(1).hours.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
